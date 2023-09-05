fd386e58a14d6eeadfd011dcc9740854a82a0276
Alex Sifuna <aleksifuna@gmail.com>
2023-09-05 22:58:43 +0000

    added a function that prints the last digits of a number

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     last_digit = "{}".format(str(number)[-1])
     if ord(last_digit) > 47 and ord(last_digit) < 58:
-        print(last_digit)
+        print(last_digit, end='')
         return (int(last_digit))
     else:
         return (None)
