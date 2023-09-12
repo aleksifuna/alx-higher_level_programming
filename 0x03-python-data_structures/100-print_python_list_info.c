934bbd8b27f242d807d9475a0d472f2ae5bb2c2f
Alex Sifuna <aleksifuna@gmail.com>
2023-09-12 13:12:50 +0000

    added a function that prints some basic information of a linked list

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <Python.h>
-#include "lists.h"
 /**
  * print_python_list_info - prints some basic information for a python list
  * @p: pointer to a python object
@@ -9,10 +8,13 @@ void print_python_list_info(PyObject *p)
 {
 	int i;
 	PyListObject *list;
-
+	PyObject *x;
 	list = (PyListObject *)p;
 	printf("[*] Size of the Python List = %ld\n", list->ob_base.ob_size);
 	printf("[*] Allocated = %ld\n", list->allocated);
 	for (i = 0; i < list->ob_base.ob_size; i++)
-		printf("Element %d: %s\n", i, list->ob_item[i]->ob_type->tp_name);
+	{
+		x = PyList_GetItem(p, i);
+		printf("Element %d: %s\n", i, (Py_TYPE(x))->tp_name);
+	}
 }
