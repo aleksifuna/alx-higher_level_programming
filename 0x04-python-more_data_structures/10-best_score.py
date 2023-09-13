8cd5c6d8440afae95f4ad30718cff416c133f0f3
Alex Sifuna <aleksifuna@gmail.com>
2023-09-13 19:44:55 +0000

    added a function that returns a key with the biggest value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is not None:
         comp = a_dictionary[list(a_dictionary)[0]]
+        key = list(a_dictionary)[0]
         for k, v in a_dictionary.items():
             if v > comp:
                 key = k
