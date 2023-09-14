fbb36b08f8b207aeb5daa1079e1beb89d729d72a
Alex Sifuna <aleksifuna@gmail.com>
2023-09-14 01:14:31 +0000

    added a function that prints some basic info of a list

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -14,7 +14,10 @@ void print_python_list(PyObject *p)
 	printf("[*] Size of the Python List = %ld\n", list->ob_base.ob_size);
 	printf("[*] Allocated = %ld\n", list->allocated);
 	for (i = 0; i < list->ob_base.ob_size; i++)
+	{
 		printf("Element %d: %s\n", i, list->ob_item[i]->ob_type->tp_name);
+		if ((list->ob_item[i]->ob_type->tp_name)[0] == 'b')
+			print_python_bytes(ob_item[i]);
 }
 /**
  * print_python_bytes - prints some basic info of a string byte
