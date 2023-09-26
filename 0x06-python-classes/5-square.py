577823dfba0c44c1c37c058a08582d85147e2a61
Alex Sifuna <aleksifuna@gmail.com>
2023-09-26 22:04:50 +0000

    added a class square with methods to calculate the area and print #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""this file contains our square class """
+
+
+class Square:
+    """A class to represent a square."""
+    def __init__(self, size=0):
+        """ Initializes a square instance.
+
+        Args:
+            size: the size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """ Retrieves attribute size.
+        Returns:
+            size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """validates the value of size
+        Args:
+            value: the size of the square.
+
+        Raises:
+            TypeError: if size if not an integer.
+            ValueError: if size is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """calculates the area of a square.
+
+        Returns:
+            area of the square
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """prints to stdout the square with #
+
+        """
+        the_size = self.__size
+        if the_size == 0:
+            print()
+        else:
+            for i in range(the_size):
+                for i in range(the_size):
+                    print("#", end="")
+                print()
