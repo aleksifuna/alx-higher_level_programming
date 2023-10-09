a78ff4aef0b3e66c9f05683fc2f61059644327af
Alex Sifuna <aleksifuna@gmail.com>
2023-10-09 18:49:59 +0000

    added a public instance method in our clas BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module supplies one class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class with a public instance method"""
+    def area(self):
+        """Raises an Exeption with custom message"""
+        raise Exception("area() is not implemented")
