ada09ff6192a8d635e874110d87a7c19b5128bea
Alex Sifuna <aleksifuna@gmail.com>
2023-10-10 23:40:19 +0000

    added a clss student with improvements

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This module supplies one class Student """
+
+
+class Student:
+    """ Define a student class togethor with the attributes and methods """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the class students
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves and returns a dictionary representation of a student
+        """
+        dic_rep = self.__dict__
+        if type(attrs) is list:
+            return {key: dic_rep[key] for key in attrs if key in dic_rep}
+        return self.__dict__
