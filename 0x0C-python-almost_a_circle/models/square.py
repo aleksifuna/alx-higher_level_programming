010f165b3665a91bc2da1e13890f643895f57d50
Alex Sifuna <aleksifuna@gmail.com>
2023-10-13 12:32:05 +0000

    added a method to square class for updating attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,9 +13,7 @@ class Square(Rectangle):
         """
         Initializes an instance of class Square
         """
-        width = size
-        height = size
-        super().__init__(width, height, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """
@@ -26,3 +24,43 @@ class Square(Rectangle):
         c = self.x
         d = self.y
         return "[Square] ({}) {}/{} - {}".format(a, c, d, b)
+
+    @property
+    def size(self):
+        """
+        Retieves the attribute size
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Validated and sets the attribute size
+        """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        updates attributes of class instance
+        """
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            if kwargs is not None:
+                for key, value in kwargs.items():
+                    if key == "id":
+                        self.id = value
+                    if key == "size":
+                        self.size = value
+                    if key == "y":
+                        self.y = value
+                    if key == "x":
+                        self.x = value
