ed28d9995fcb007a398677d500d6a4ea23bbf759
Alex Sifuna <aleksifuna@gmail.com>
2023-11-14 19:42:29 +0000

    added a script that creates a table and adds records to it

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- creates a table second_table in db and adds records to it
+
+-- creating of the table
+CREATE TABLE second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+
+-- adding records to the table
+INSERT INTO second_table  VALUES(1, "John", 10);
+INSERT INTO second_table  VALUES(2, "Alex", 3);
+INSERT INTO second_table  VALUES(3, "Bob", 14);
+INSERT INTO second_table  VALUES(4, "George", 8);
