c661fcd08250d7e12ca3c5944e19db85bfc6ce5e
Alex Sifuna <aleksifuna@gmail.com>
2023-11-15 22:48:06 +0000

    added a script that lists all shows from a db

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in the db
+
+SELECT s.title, g.genre_id FROM tv_shows s
+LEFT JOIN tv_show_genres g
+ON s.id=g.show_id
+WHERE g.genre_id IS NULL
+ORDER BY s.title, g.genre_id ASC;
