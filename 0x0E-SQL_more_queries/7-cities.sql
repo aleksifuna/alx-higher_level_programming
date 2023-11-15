c122e6955853919fe5958eb6a309da2e49ada43e
Alex Sifuna <aleksifuna@gmail.com>
2023-11-15 21:27:55 +0000

    added a script that creates a db and a table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- creates database hbtn_0d_usa and table cities in that db
+-- if db and table already exists it will not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT AUTO_INCREMENT UNIQUE PRIMARY KEY NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256),
+	FOREIGN KEY (state_id) REFERENCES states(id)
+	);
