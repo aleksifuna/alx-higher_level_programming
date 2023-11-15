9e19f4e568b7a0d229299efc0215cbcf23bea360
Alex Sifuna <aleksifuna@gmail.com>
2023-11-15 20:02:53 +0000

    Added a script that creates a db and user and assign SELECT privileges to user on db

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- user_0d_2 is granted SELECT privileges only in the db
+-- user_0d_2 password should be user_0d_2_pwd
+-- if  db and user exist the code wont fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
