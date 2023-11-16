214c0c2341f15d20042910331a702e1a983d4dc4
Alex Sifuna <aleksifuna@gmail.com>
2023-11-16 01:14:58 +0000

    added a script that lists all shows and ratings

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- lists all shows from the db with its ratings
+
+SELECT t.title, SUM(r.rate) AS rating FROM tv_shows t
+JOIN tv_show_ratings r ON t.id = r.show_id
+GROUP BY t.title
+ORDER BY rating DESC;
