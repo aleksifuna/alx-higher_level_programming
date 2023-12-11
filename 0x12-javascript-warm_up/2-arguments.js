0bdbc4d17a2aa8483e844cff54e61e82211101ba
Alex Sifuna <aleksifuna@gmail.com>
2023-12-11 20:04:44 +0000

    added a fnction that counts number of arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 const argCount = process.argv.length - 2;
 if (argCount === 0) {
   console.log('No argument');
-} else {
+} else if (argCount === 1) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
