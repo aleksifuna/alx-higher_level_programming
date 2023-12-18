0926194870c471c708ad7fe65c00b8b1e51a8361
aleksifuna <aleksifuna@gmail.com>
2023-12-18 01:04:03 +0000

    Added a script that adds the state object Louisiana to the db

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+prints the first state object from the db
+"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    session = sessionmaker(bind=engine)
+    session = session()
+    Base.metadata.create_all(engine)
+    add_state = State(name="Louisiana")
+    session.add(add_state)
+    session.commit()
+    print(add_state.id)
+    session.close()
