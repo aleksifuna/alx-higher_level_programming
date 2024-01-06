a8b1d9675fe2f1ac42a79ea1beb72bbe0f2fb8c8
Alex Sifuna <aleksifuna@gmail.com>
2024-01-06 07:49:42 +0000

    made some changes on the sript

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to a server and causes it to respond
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+curl -o /dev/null -sw "You find me!" 0.0.0.0:5000/catch_me
