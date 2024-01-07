982e7be6a776af5361b39ea7cf9e48db58467240
Alex <aleksifuna@gmail.com>
2024-01-07 12:52:04 -0800

    added a script that fetches a variable in a repsonse header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,6 +5,6 @@ supplies a function that fetches a variable in response header
 import sys
 import requests
 
-
-response = requests.get(sys.argv[1])
-print(response.headers['X-Request-Id'])
+if __name__ == '__main__':
+    response = requests.get(sys.argv[1])
+    print(response.headers['X-Request-Id'])
