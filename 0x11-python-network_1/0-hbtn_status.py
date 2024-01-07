9fb309b79caabfb882e272a2c67a0622345be52a
Alex <aleksifuna@gmail.com>
2024-01-07 07:07:16 -0800

    Added a script that fetches a website

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module supplies a script that fetches a url """
+import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    content = response.read()
+    print('Body reponse:')
+    print('\t- type: ' + str(type(content)))
+    print('\t- content: ' + str(content))
+    print('\t- utf8 content: ' + content.decode('utf-8'))
